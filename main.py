bd24b8f5f9f498a58859de23bba9f4e5a30b9c2c
Nathan Alderson <nathan.alderson@gmail.com>
2019-03-14 22:51:22 -0500

    Prompt for password

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import *
+
 app = Flask(__name__)
 
+app.secret_key = r"\G)cTBS.iX3]&r6{:e'v5XNksS}5E:D6CP"
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/page-body", methods=['POST'])
+def page_body():
+    if request.form["password"] == "pickles":
+        return render_template('page_body.html')
+    else:
+        abort(401)
