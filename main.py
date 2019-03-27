b6b29ae748bba5b7be0b26e85477158ee4c79d10
Nathan Alderson <nathan.alderson@gmail.com>
2019-03-26 23:15:33 -0500

    Lots more content

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 app.secret_key = r"\G)cTBS.iX3]&r6{:e'v5XNksS}5E:D6CP"
 
+TAKEN = [4,11,15,21,22,37,38,50,51,55,84,89,92,94,110,122,144,150]
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -11,7 +13,7 @@ def index():
 @app.route("/page-body", methods=['POST'])
 def page_body():
     if request.form["password"] == "pickles":
-        return render_template('index2.html')
+        return render_template('index2.html', taken=TAKEN)
     else:
         abort(401)
 
@@ -20,7 +22,7 @@ def bypass_password():
     if app.config['DEBUG']:
         return render_template('index.html',
                 bypass_password=True,
-                protected_content=render_template('index2.html'))
+                protected_content=render_template('index2.html', taken=TAKEN))
     else:
         abort(404)
 
