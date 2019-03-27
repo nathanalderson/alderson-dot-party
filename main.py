480994cc956ce733f66c3f9cee0eb1aa278e9ab2
Nathan Alderson <nathan.alderson@gmail.com>
2019-03-27 18:22:38 -0500

    Make taken list dynamic

--- main.py
+++ main.py
@@ -4,23 +4,9 @@ app = Flask(__name__)
 
 app.secret_key = r"\G)cTBS.iX3]&r6{:e'v5XNksS}5E:D6CP"
 
-TAKEN = [
-    1, 2, 3, 4, 5, 6, 7, 8, 
-    11, 12, 13, 15, 16, 19,
-    20, 21, 22, 23, 24,
-    30, 35, 37, 38,
-    41, 45, 48,
-    50, 51, 55,
-    68,
-    75, 78,
-    81, 84, 89,
-    92, 94, 98,
-    100, 110, 111,
-    122,
-    138,
-    143,
-    150,
-]
+def get_taken():
+    with open("taken.txt", 'r') as f:
+        return [int(i) for i in f.read().split()]
 
 @app.route("/")
 def index():
@@ -29,7 +15,7 @@ def index():
 @app.route("/page-body", methods=['POST'])
 def page_body():
     if request.form["password"].lower() == "keller":
-        return render_template('index2.html', taken=TAKEN)
+        return render_template('index2.html', taken=get_taken())
     else:
         abort(401)
 
@@ -38,7 +24,7 @@ def bypass_password():
     if app.config['DEBUG']:
         return render_template('index.html',
                 bypass_password=True,
-                protected_content=render_template('index2.html', taken=TAKEN))
+                protected_content=render_template('index2.html', taken=get_taken()))
     else:
         abort(404)
 
